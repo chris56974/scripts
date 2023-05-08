26c1c72285b5c030b6b1f0b5e195f69e6f417a08
Chris Straka <skylake112@outlook.com>
2023-05-07 19:10:45 -0600

    make updates

--- powershell/setupAlgoQuestion.ps1
+++ powershell/setupAlgoQuestion.ps1
@@ -4,8 +4,4 @@ if (New-Item README.md) {
   Write-Output "# $currentDirectoryName`n`n## IO`n`n## External" >> README.md
 }
 
-cargo init rust --vcs none
-
-dotnet new console -o csharp
-
 Clear-Host
